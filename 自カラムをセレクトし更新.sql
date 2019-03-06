03ef4cb476f9a6faf763f06461c32988b3298846
yuzuk <kikuchi.45@gmail.com>
2019-03-06 16:10:15 +0900

    メールアドレスの更新

--- 自カラムをセレクトし更新.sql
+++ 自カラムをセレクトし更新.sql
@@ -11,3 +11,18 @@ FROM
   ) A
 ) B
 WHERE mst_users.id = B.id
+
+--アンダースコア(_)の他に、アットマーク(@)でも分割する
+--サブクエリのA.arrは、{mamoru,fuji,32,example.com}のように返ってくるので、
+--配列を組み替えて文字列結合した結果(mamoru_fuji_32@example.com)で更新する
+
+UPDATE mst_users
+SET email = B.uname
+FROM
+(
+  SELECT id, COALESCE(arr[3] || '_' || arr[1] || '_' || arr[2]  || '@' || arr[4], email) as uname FROM
+  (
+    SELECT id, email, regexp_split_to_array(email, '(_|@)') as arr FROM mst_users
+  ) A
+) B
+WHERE mst_users.id = B.id
